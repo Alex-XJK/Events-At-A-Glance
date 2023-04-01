03060923ef505c31d29c5a99b4517ec0c1196702
Restyled.io <commits@restyled.io>
2023-04-01 06:03:43 +0000

    Restyled by isort

--- server.py
+++ server.py
@@ -9,10 +9,10 @@ Read about it online.
 """
 import os
 
+from flask import Flask, Response, g, redirect, render_template, request
 # accessible as a variable in index.html:
 from sqlalchemy import *
 from sqlalchemy.pool import NullPool
-from flask import Flask, request, render_template, g, redirect, Response
 
 tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "templates")
 app = Flask(__name__, template_folder=tmpl_dir)
